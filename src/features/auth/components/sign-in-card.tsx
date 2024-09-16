6bffa93f00852b78d2bdb570d0fe305476e008f4
Jorge Matos <jisapjisap@gmail.com>
2024-09-16 17:05:40 +0200

    Auth

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,7 +6,10 @@ import {
   CardHeader, 
   CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-
+import { Separator } from "@/components/ui/separator"
+import { FcGoogle } from "react-icons/fc"
+import { FaGithub } from "react-icons/fa"
+ 
 
 
 export const SignInCard = () => {
@@ -16,10 +19,10 @@ export const SignInCard = () => {
         <CardTitle>
           Login to continue
         </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
       </CardHeader>
-      <CardDescription>
-        Use your email or another service to continue
-      </CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
         <form className="space-y-2.5">
           <Input 
@@ -47,6 +50,32 @@ export const SignInCard = () => {
             Continue
           </Button>
         </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle  className="size-5 absolute top-2.5 left-2.5"/>
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => { }}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-2.5 left-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account ? <span className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
+        </p>
       </CardContent>
     </Card>
   )
