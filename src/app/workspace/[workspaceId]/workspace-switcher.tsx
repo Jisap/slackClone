59d10df977f5c26882c43b53a37a17c28927addc
Jorge Matos <jisapjisap@gmail.com>
2024-09-26 18:49:38 +0200

    sidebar

---
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -0,0 +1,25 @@
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
+
+export const WorkspaceSwitcher = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+          A
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  )
+}
