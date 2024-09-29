d3c80ddca3bffcd486b84476ca196d2add26dc47
Jorge Matos <jisapjisap@gmail.com>
2024-09-29 19:02:49 +0200

    worspacesidebar

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,7 +1,7 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { Loader } from "lucide-react";
+import { AlertTriangle, Loader } from "lucide-react";
 
 
 
@@ -20,7 +20,17 @@ export const WorkspaceSidebar = () => {
   if(memberIsLoading || workspaceIsLoading) {
     return (
       <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
-        <Loader />
+        <Loader className="size-10 animate-spin text-white" />
+      </div>)
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-10 text-white" />
+        <p className="text-white text-sm">
+          Workspace not found
+        </p>
       </div>)
   }
 
