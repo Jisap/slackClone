a72a9db005a8e820035c4c25b0f2fb7a1cadf9e3
Jorge Matos <jisapjisap@gmail.com>
2024-10-02 16:52:58 +0200

    workspaceSection

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,24 +1,33 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader, MessageSquareText, SendHorizonal, Sidebar } from "lucide-react";
+import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal, Sidebar } from "lucide-react";
 import { WorkspaceHeader } from "./workspaceHeader";
 import SidebarItem from "./sidebar-Item";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { WorkspaceSection } from "./WorkspaceSection";
 
 
 
 export const WorkspaceSidebar = () => {
 
   const workspaceId = useWorkspaceId();                      // Obtenemos el id del workspace contenido en los params
+  
   const { 
     data: member, 
     isLoading: memberIsLoading 
   } = useCurrentMember({ workspaceId });                     // Nos permite saber si el usuario logueado pertenece/es miembro al workspace 
+  
   const {
     data: workspace,
     isLoading: workspaceIsLoading
   } = useGetWorkspace({ id: workspaceId });                  // Obtenemos los datos ese workspace activo perteneciente al usuario logueado
   
+  const {
+    data: channels,
+    isLoading: channelsLoading,                            
+  } = useGetChannels({ workspaceId })                       // Obtenemos los datos de los channels del workspace
+
   if(memberIsLoading || workspaceIsLoading) {
     return (
       <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
@@ -53,6 +62,22 @@ export const WorkspaceSidebar = () => {
           icon={SendHorizonal}
           id="drafts"
         />
+
+      <WorkspaceSection
+        label="Channels"
+        hint="New channel"
+        onNew={() => {}}
+      >
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
+          />
+        ))}
+      </WorkspaceSection>
+
       </div>
     </div>
   )
