ed383d7c313757657b39159651163c55b5d25c1d
Jorge Matos <jisapjisap@gmail.com>
2024-10-10 18:29:52 +0200

    Header

--- src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { FaChevronDown } from "react-icons/fa"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog"
+import { TrashIcon } from "lucide-react"
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+
 
 
 interface HeaderProps {
@@ -5,7 +21,85 @@ interface HeaderProps {
 }
 
 export const Header = ({ name }: HeaderProps) => {
+
+  const [value, setValue] = useState(name);
+  const [editOpen, setEditOpen] = useState(false);
+
   return (
-    <div>{name}</div>
+    <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="text-lg font-semibold px-2 overflow-hidden w-auto"
+            size="sm"
+          >
+            <span className="truncate"># {name}</span>
+            <FaChevronDown className="size-2.5 ml-2"/>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle>
+              # {name}
+            </DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+              <DialogTrigger asChild>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Channel name</p>
+                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                      Edit
+                    </p>
+                  </div>
+                  <p className="text-sm"># {name}</p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>
+                    Rename this channel
+                  </DialogTitle>
+                </DialogHeader>
+                <form className="space-y-4">
+                  <Input 
+                    value={value}
+                    disabled={false}
+                    onChange={() => {}}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="Channel name"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        variant="outline"
+                        disabled={false}
+                      >
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button disabled={false}>
+                      Save
+                    </Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+
+            <button
+              className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600"
+            >
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete Channel</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
