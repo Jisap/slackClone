dcff58502ea5772113b8a40325aefab7613fb356
Jorge Matos <jisapjisap@gmail.com>
2024-10-13 18:29:25 +0200

    editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react'
 import Quill, { QuillOptions } from 'quill'
 import "quill/dist/quill.snow.css"
+import { Button } from './ui/button';
+import { PiTextAa } from 'react-icons/pi';
+import { ImageIcon, Smile } from 'lucide-react';
+import { MdSend } from 'react-icons/md';
 
 
 const Editor = () => {
@@ -35,9 +39,39 @@ const Editor = () => {
         <div 
           ref={containerRef}
           className='h-full ql-custom'  
-        >
-          Editor
-        </div>
+        />
+        <div className='flex px-2 pb-2 z-[5]'>
+          <Button
+            disabled={false}
+            variant='ghost'
+            size="iconSm"
+            onClick={() => {}}
+          >
+            <PiTextAa className='size-4' />
+          </Button>
+          <Button
+            disabled={false}
+            variant='ghost'
+            size="iconSm"
+            onClick={() => { }}
+          >
+            <Smile className='size-4' />
+          </Button>
+          <Button
+            disabled={false}
+            variant='ghost'
+            size="iconSm"
+            onClick={() => { }}
+          >
+            <ImageIcon className='size-4' />
+          </Button>
+          <Button
+            size="iconSm"
+            className='ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white'
+          >
+            <MdSend className='size-4' />
+          </Button>
+        </div>     
       </div>
     </div>
   )
