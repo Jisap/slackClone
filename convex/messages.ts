2b336d7ef8981ef5bca39018d54a28c8660735d4
Jorge Matos <jisapjisap@gmail.com>
2024-10-23 16:58:15 +0200

    useGetMessages

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,8 +1,9 @@
 import { v } from "convex/values";
-import { mutation, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { Id } from "./_generated/dataModel";
 import { timeStamp } from "console";
+import { paginationOptsValidator } from "convex/server";
 
 const populateUser = (ctx: QueryCtx, userId: Id<"users">) => { // Busca un registro específico utilizando el ID proporcionado.
   return ctx.db.get(userId)
@@ -18,12 +19,15 @@ const populateReactions = (ctx: QueryCtx, messageId: Id<"messages">) => { // Bus
     .collect()
 }
 
-const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => { //  Trata de obtener todos los mensajes que forman parte de un hilo (thread) de conversación, 
-  const messages = await ctx.db                                              //  Osea aquellos mensajes que tienen un mensaje "padre" específico, identificado por messageId. 
+const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => { // Trata de obtener todos los mensajes que forman parte de un hilo (thread) de conversación,
+                                                                             // Osea aquellos mensajes que tienen un mensaje "padre" específico, identificado por messageId.
+  // 1. Obtiene todos los mensajes que pertenecen al hilo
+  const messages = await ctx.db                                               
     .query("messages")
     .withIndex("by_parent_message_id", (q) => q.eq("parentMessageId", messageId))
     .collect()
   
+  // 2. Si no hay mensajes en el hilo, retorna un objeto con valores por defecto
   if(messages.length === 0){
     return {
       count: 0,
@@ -32,19 +36,25 @@ const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => { // 
     }
   }
 
+  // 3. Obtiene el último mensaje del hilo
   const lastMessages = messages[messages.length - 1];
+
+  // 4. Member que escribió el último mensaje
   const lastMessageMember = await populateMember(ctx, lastMessages.memberId)
 
+  // 5. Si no se encuentra el member, retorna un objeto con valores por defecto
   if(!lastMessageMember){
     return{
-      count: messageId.length,
+      count: 0,
       image: undefined,
       timeStamp: 0,
     }
   }
 
+  // 6. Si se encontro el member se busca el usuario que lo escribio
   const lastMessageUser = await populateUser(ctx, lastMessageMember.userId)
   
+  // 7. Retorna la cantidad de mensajes, la imagen del usuario que escribió el último mensaje, y el timestamp del último mensaje
   return {
     count: messageId.length,
     image: lastMessageUser?.image,
@@ -64,6 +74,46 @@ const getMember = async( // Busca un registro específico utilizando el ID del w
     )
     .unique();
 }
+
+export const get = query({ // Endpoint para manejar la recuperación de mensajes en diferentes contextos (por canal, conversación o mensajes en un hilo)
+  args: {
+    channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
+    paginationOpts: paginationOptsValidator
+  },
+  handler: async (ctx, args) => { 
+    const userId = await getAuthUserId(ctx);
+    if(!userId){
+      throw  new Error("Unauthorized")
+    }
+
+    let _conversationId = args.conversationId
+
+    if(!args.conversationId && !args.channelId && args.parentMessageId){  // Si no tenemos el id de la conversation y tampoco el id del channel
+      const parentMessage = await ctx.db.get(args.parentMessageId)        // se obtiene el ID de la conversación a partir del mensaje padre:
+
+      if(!parentMessage){
+        throw new Error("Parent message not found")
+      }
+
+      _conversationId = parentMessage.conversationId                      // Se asigna el id del parentMessage al de la conversation
+    }
+
+    const results = await ctx.db                                          
+      .query("messages")                                                  // Se consulta a la tabla messages
+      .withIndex("by_channel_id_parent_message_id_conversation", q =>     // con el índice definido en el schema
+        q                                                                 // para filtrar mensajes según el channelId, parentMessageId, y conversationId.
+          .eq("channelId", args.channelId)
+          .eq("parentMessageId", args.parentMessageId)
+          .eq("conversationId", _conversationId)
+      )
+        .order("desc")
+        .paginate(args.paginationOpts)
+
+    return results // Finalmente, se devuelven los resultados paginados de la consulta a la tabla "messages"
+  }
+})
 
 export const create = mutation({
   args: {
