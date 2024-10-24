cc6541770a64f64259b72140ac4e1c7dd064523a
Jorge Matos <jisapjisap@gmail.com>
2024-10-24 15:49:26 +0200

    messageList

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,4 +1,5 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
+import { format, isToday, isYesterday } from "date-fns";
 
 interface MessageListProps {
   memberName?: string;
@@ -12,6 +13,13 @@ interface MessageListProps {
   canLoadMore: boolean;
 }
 
+const formatDateLabel = (dateStr: string) => {
+  const date = new Date(dateStr);
+  if(isToday(date)) return "Today";
+  if(isYesterday(date)) return "Yesterday";
+  return format(date, "EEEE, MMMM d");
+}
+
 export const MessageList = ({
   memberName,
   memberImage,
@@ -23,13 +31,50 @@ export const MessageList = ({
   isLoadingMore,
   canLoadMore,  
 }: MessageListProps) => {
+
+  const groupedMessages = data?.reduce( // Mensajes agrupados por fechas
+    (groups, message) => {
+      const date = new Date(message._creationTime);
+      const dateKey = format(date, "yyyy-MM-dd");
+      if(!groups[dateKey]){
+        groups[dateKey] = [];
+      }
+      groups[dateKey].unshift(message);
+      return groups;
+    },
+    {} as Record<string, typeof data>
+  )
+
+  // Object convierte un objeto 
+  // const obj = {
+  //   "2024-10-24": [mensaje1, mensaje2],
+  //   "2024-10-23": [mensaje3, mensaje4]
+  // }
+  // en un array de pares[clave, valor] -> 
+  // [
+  //   ["2024-10-24", [mensaje1, mensaje2]],
+  //   ["2024-10-23", [mensaje3, mensaje4]]
+  // ]
+
   return (
     <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
-      {data?.map((message) => (
-        <div>
-          {JSON.stringify(message)}
+     {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+        <div key={dateKey}>
+          <div className="text-center my-2 relative">
+            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+            <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+              {formatDateLabel(dateKey)}
+            </span>
+          </div>
+          {messages.map((message, index) => {
+            return (
+              <div key={message._id}>
+                {JSON.stringify(message)}
+              </div>
+            )
+          })}
         </div>
-      ))}
+     ))}
     </div>
   )
 }
