f9d28f23010bde1b9ffacdccd5fc643b425b8b7f
Jorge Matos <jisapjisap@gmail.com>
2024-11-05 16:47:09 +0100

    Profile

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,4 +1,7 @@
 import { Id } from "convex/_generated/dataModel"
+import { useGetMember } from "../api/use-get-member"
+import { Button } from "@/components/ui/button"
+import { AlertTriangle, Loader, XIcon } from "lucide-react"
 
 
 interface ProfileProps {
@@ -7,6 +10,49 @@ interface ProfileProps {
 }
 
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
+
+  const { data: member, isLoading: isLoadingMember } = useGetMember({ id: memberId });
+
+  if (isLoadingMember) {
+    return (
+      <div className="h-full flex flex-col">
+        <div className="flex justify-between items-center px-4  h-[49px] border-b">
+          <p className="text-lg font-bold">Profile</p>
+          <Button
+            onClick={onClose}
+            size="iconSm"
+            variant="ghost"
+          >
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className='flex flex-col gap-y-2 h-full items-center justify-center'>
+          <Loader className="animate-spin size-5 text-muted-foreground" />
+          <p className="text-sm text-muted-foreground">Profile not found</p>
+        </div>
+      </div>
+    )
+  }
+
+  if (!member) {
+    <div className="h-full flex flex-col">
+      <div className="flex justify-between items-center px-4  h-[49px] border-b">
+        <p className="text-lg font-bold">Profile</p>
+        <Button
+          onClick={onClose}
+          size="iconSm"
+          variant="ghost"
+        >
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+      <div className='flex flex-col gap-y-2 h-full items-center justify-center'>
+        <AlertTriangle className="size-5 text-muted-foreground" />
+        <p className="text-sm text-muted-foreground">Member not found</p>
+      </div>
+    </div>
+  }
+
   return (
     <div>
       profile
