f21b866c73818f3809bc50dc9f73948c88bcb8d8
Jorge Matos <jisapjisap@gmail.com>
2024-11-07 12:50:46 +0100

    profile funcionality

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -10,6 +10,8 @@ import { useUpdateMember } from "../api/use-update-message"
 import { useRemoveMember } from "../api/use-remove-member"
 import { useCurrentMember } from "../api/use-current-member"
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { toast } from "sonner"
+import { useConfirm } from "@/hooks/use-confirm"
 
 
 interface ProfileProps {
@@ -20,12 +22,75 @@ interface ProfileProps {
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
 
   const workspaceId = useWorkspaceId(); 
+
+  const [LeaveDialog, confirmLeave] = useConfirm(
+    "Leave the workspace ?",
+    "Are you sure you want to leave the workspace ?"
+  );
+
+  const [RemoveDialog, confirmRemove] = useConfirm(
+    "Remove member ?",
+    "Are you sure you want to remove this member ?"
+  );
+
+  const [UpdateDialog, confirmUpdate] = useConfirm(
+    "Change role ?",
+    "Are you sure you want to change the role ?"
+  );
+
   const { data: currentMember, isLoading: isLoadinCurrentMember } = useCurrentMember({workspaceId}); // Nos permite saber si el usuario logueado pertenece al workspace
 
   const { data: member, isLoading: isLoadingMember } = useGetMember({ id: memberId });
 
   const { mutate: updateMember, isPending: isUpdatingMember } = useUpdateMember()
   const { mutate: removeMember, isPending: isRemovingMember } = useRemoveMember()
+
+  const onRemove = async() => {
+    const ok = await confirmRemove();
+    if (!ok) return;
+
+    removeMember({ id: memberId },{
+      onSuccess: () => {
+        toast.success("Member removed successfully");
+        onClose();
+      },
+      onError: (error) => {
+        toast.error("Failed to remove member");
+      }
+    })
+  }
+
+  const onLeave = async() => {
+    const ok = await confirmLeave();
+    if (!ok) return;
+
+
+    removeMember({ id: memberId }, {
+      onSuccess: () => {
+        toast.success("You left the workspace");
+        onClose();
+      },
+      onError: (error) => {
+        toast.error("Failed to leave the workspace");
+      }
+    })
+  }
+
+  const onUpdate = async(role: "admin" | "member") => {
+    const ok = await confirmUpdate();
+    if (!ok) return;
+
+    updateMember({ id: memberId, role }, {
+      onSuccess: () => {
+        toast.success("Role changed successfully");
+        onClose();
+      },
+      onError: (error) => {
+        toast.error("Failed to change role");
+      }
+    })
+  }
+
 
   if (isLoadingMember || isLoadinCurrentMember) {
     return (
@@ -71,60 +136,80 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
     const avatarFallback = member.user.name?.[0] ?? "M"
     
     return (
-      <div className="h-full flex flex-col">
-        <div className="flex justify-between items-center px-4  h-[49px] border-b">
-          <p className="text-lg font-bold">Profile</p>
-          <Button
-            onClick={onClose}
-            size="iconSm"
-            variant="ghost"
-          >
-            <XIcon className="size-5 stroke-[1.5]" />
-          </Button>
-        </div>
-        <div className='flex flex-col items-center justify-center p-4'>
-          <Avatar className="max-w-[256px] max-h-[256px] size-full">
-            <AvatarImage src={member.user.image} />
-            <AvatarFallback className="aspect-square text-6xl">
-              {avatarFallback}
-            </AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex flex-col p-4">
-          <p className="text-xl font-bold">{member.user.name}</p>
-          {currentMember?.role === "admin" &&                           // Si el miembro actual es admin,
-            currentMember._id !== member._id ? (                        // y es distinto al miembro que se pasa por argumentos -> se muestra el boton de cambiar el rol
-              <div className="flex items-center gap-2 mt-4">
-                <Button variant="outline" className="w-full capitalize">
-                  {member.role} <ChevronDownIcon className="size-4" />
-                </Button>
-                <Button variant="outline" className="w-full">
-                  Remove
-                </Button>
+      <>
+        <RemoveDialog />
+        <LeaveDialog />
+        <UpdateDialog />
+        <div className="h-full flex flex-col">
+          <div className="flex justify-between items-center px-4  h-[49px] border-b">
+            <p className="text-lg font-bold">Profile</p>
+            <Button
+              onClick={onClose}
+              size="iconSm"
+              variant="ghost"
+            >
+              <XIcon className="size-5 stroke-[1.5]" />
+            </Button>
+          </div>
+          <div className='flex flex-col items-center justify-center p-4'>
+            <Avatar className="max-w-[256px] max-h-[256px] size-full">
+              <AvatarImage src={member.user.image} />
+              <AvatarFallback className="aspect-square text-6xl">
+                {avatarFallback}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="flex flex-col p-4">
+            <p className="text-xl font-bold">{member.user.name}</p>
+            {currentMember?.role === "admin" &&                            // Si el miembro actual (logueado y pertenece al workspace) es admin,
+              currentMember?._id !== member._id ? (                        // y es distinto al miembro que se pasa por argumentos -> se muestra el boton de cambiar el rol
+                <div className="flex items-center gap-2 mt-4">
+                  <Button variant="outline" className="w-full capitalize">
+                    {member.role} <ChevronDownIcon className="size-4" />
+                  </Button>
+                  <Button 
+                    variant="outline" 
+                    className="w-full"
+                    onClick={onRemove}
+                  >
+                    Remove
+                  </Button>
+                </div>
+              ) : currentMember?._id === memberId  &&    // Si el miembro actual es el miembro que se pasa por argumentos (se clikea a si mismo)
+                    currentMember?.role !== "admin" ? (  // y no es admin -> se muestra el boton de dejar el workspace (solo sucede si eres un miembro invitado)
+                      <div className="mt-4">
+                        <Button 
+                          variant="outline" 
+                          className="w-full"
+                          onClick={onLeave}  
+                        >
+                          Leave
+                        </Button>
+                      </div>
+                    ) : null
+            }
+          </div>
+          <Separator />
+          <div className="flex flex-col p-4">
+            <p className="text-sm font-bold mb-4">
+              Contact Information
+            </p>
+            <div className="flex items-center gap-2">
+              <div className="size-9 rounded-md bg-muted flex items-center justify-center">
+                <MailIcon  className="size-4"/>
               </div>
-            ) : null
-          }
-        </div>
-        <Separator />
-        <div className="flex flex-col p-4">
-          <p className="text-sm font-bold mb-4">
-            Contact Information
-          </p>
-          <div className="flex items-center gap-2">
-            <div className="size-9 rounded-md bg-muted flex items-center justify-center">
-              <MailIcon  className="size-4"/>
-            </div>
-            <div className="flex flex-col">
-              <p className="text-[13px] font-semibold text-muted-foreground">
-                Email address
-              </p>
-              <Link href={`mailto:${member.user.email}`} className="text-sm hover:underline text-[#1264a3]">
-                {member.user.email}
-              </Link>
+              <div className="flex flex-col">
+                <p className="text-[13px] font-semibold text-muted-foreground">
+                  Email address
+                </p>
+                <Link href={`mailto:${member.user.email}`} className="text-sm hover:underline text-[#1264a3]">
+                  {member.user.email}
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     )
   }
 
