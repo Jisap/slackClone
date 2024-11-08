bf71a1dbd9f0bc32d439e121b6a479630d9db3b2
Jorge Matos <jisapjisap@gmail.com>
2024-11-08 19:59:52 +0100

    fix deployment

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -49,7 +49,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
       }
 
       if(image){                                                       // Si hay una imagen adjunta
-        const url = await generateUploadUrl({ throwError: true }); // se genera una URL de subida a convex     
+        const url = await generateUploadUrl({ throwError: true });     // se genera una URL de subida a convex     
       
         if(!url){
           throw new Error("Url not found");
